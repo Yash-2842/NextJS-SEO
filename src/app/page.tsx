b88d868fdb72b95486b081df95e35bf1460c8144
Yash-2842 <84618410+Yash-2842@users.noreply.github.com>
2023-11-03 11:53:47 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div>
+      <Head>
+        
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1006603732938230" crossorigin="anonymous"></script>
+    </Head>
       <IFrameLoader />
     </div>
   );
